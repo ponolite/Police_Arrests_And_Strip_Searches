dd7c73f98b55fda9b7c023fda4628e4f5ea5b66e
ponolite <147739831+ponolite@users.noreply.github.com>
2024-01-22 15:47:10 -0500

    Update 02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -16,10 +16,10 @@ library(janitor)
 
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_police_arrests_strip_data <- read_csv("inputs/data/raw_police_arrests_strip_data.csv")
 
-cleaned_data <-
-  raw_data |>
+cleaned_arrests_strip_data <-
+  raw_police_arrests_strip_data |>
   janitor::clean_names() |>
   select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
   filter(wing_width_mm != "caw") |>
@@ -46,4 +46,4 @@ cleaned_data <-
   tidyr::drop_na()
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_arrests_strip_data,"inputs/data/cleaned_arrests_strip_data.csv")

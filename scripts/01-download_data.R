d08e9fed624c194fd39f0482149e0473c00a7941
ponolite <147739831+ponolite@users.noreply.github.com>
2024-01-22 15:34:51 -0500

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,39 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads dataset of police arrests and strip searches throughout Toronto 
+# Author: Quang Mai
+# Date: 21 January 2024 
+# Contact: q.mai@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: none
 
 
 #### Workspace setup ####
 library(opendatatoronto)
+library(dplyr)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+#From https://open.toronto.ca/dataset/police-race-and-identity-based-data-collection-arrests-strip-searches/
+
+# download data package 
+
+package <- show_package("police-race-and-identity-based-data-collection-arrests-strip-searches")
+package
+
+resources <- list_package_resources("police-race-and-identity-based-data-collection-arrests-strip-searches")
+resources
+
+# this package has only one dataset within it and must be selected by default
+
+raw_police_arrests_strip_data <- get_resource(resource ="78ef6b68-7a88-47f9-9dab-a0f00f5d8b0d")
+raw_police_arrests_strip_data 
 
 
+#### Save Data ####
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
+write_csv (
+         x = raw_police_arrests_strip_data, 
+         file - "inputs/data/raw_police_arrests_strip_data.csv"
+)
          

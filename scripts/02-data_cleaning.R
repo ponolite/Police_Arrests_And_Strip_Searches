537b31276b7e568f91f3ed55d66156fc737e1890
Quang Mai <quangpono@gmail.com>
2024-01-23 04:32:58 +0000

    refine data cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -21,13 +21,15 @@ library(naniar)
 
 
 #### Clean data ####
+# Code of function replace_with_na referenced from https://tidyr.tidyverse.org/reference/replace_na.html
+
 raw_police_arrests_strip_data <- read_csv("inputs/data/raw_police_arrests_strip_data.csv")
 
 cleaned_arrests_strip_data <-
   raw_police_arrests_strip_data |>
-  janitor::clean_names() |>
-  select(-c(event_id, arrest_id, person_id, object_id, booked)) |>
-  rename(action_concealed = actions_at_arrest_concealed_i,
+  janitor::clean_names() |> #clean and simplify variable names 
+  select(-c(x_id, arrest_id, person_id, object_id, booked)) |> # Get rid of unnecessary variables
+  rename(action_concealed = actions_at_arrest_concealed_i, # Shorten variable names to make them readable
          action_combative = actions_at_arrest_combative,
          action_resisted = actions_at_arrest_resisted_d,
          action_mental_inst = actions_at_arrest_mental_inst,
@@ -37,12 +39,18 @@ cleaned_arrests_strip_data <-
          reason_escape = search_reason_assist_escape,
          reason_weapons = search_reason_possess_weapons,
          reason_has_evidence = search_reason_possess_evidence,
-         items_found = items_found
+         race = perceived_race
          ) |> 
-  naniar::replace_with_na(replace = list(reason_injury = c("None"),
+  relocate(event_id) |>
+  naniar::replace_with_na(replace = list(reason_injury = c("None"), # Replace the string 'None' with NA to make it easier to manipulate data later on
                                  reason_escape = c("None"),
                                  reason_weapons = c("None"),
                                  reason_has_evidence = c("None")))
+
+# Replace all "XX" and "None" from the columns arrest_loc_div and items_found with NA and 0 respectively to make the data processing easier later on
+# Code referenced from https://www.youtube.com/watch?v=g1eppE60J5g
+cleaned_arrests_strip_data$items_found[cleaned_arrests_strip_data$items_found == "None"] <- 0
+cleaned_arrests_strip_data$arrest_loc_div[cleaned_arrests_strip_data$arrest_loc_div == "XX"] <- NA
 
 
 #### Save data ####

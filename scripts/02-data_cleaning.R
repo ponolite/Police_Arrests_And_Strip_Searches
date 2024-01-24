dd432bbb921fb337a9708544f3794d0e25987720
Quang Mai <quangpono@gmail.com>
2024-01-24 08:53:27 +0000

    update new data and further clean data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -29,14 +29,9 @@ raw_police_arrests_strip_data <- read_csv("inputs/data/raw_police_arrests_strip_
 cleaned_arrests_strip_data <-
   raw_police_arrests_strip_data |>
   janitor::clean_names() |> #clean and simplify variable names 
-  select(-c(x_id, arrest_id, person_id, object_id, booked)) |> # Get rid of unnecessary variables
-  rename(action_concealed = actions_at_arrest_concealed_i, # Shorten variable names to make them readable
-         action_combative = actions_at_arrest_combative,
-         action_resisted = actions_at_arrest_resisted_d,
-         action_mental_inst = actions_at_arrest_mental_inst,
-         action_assaulted = actions_at_arrest_assaulted_o,
-         action_cooperative = actions_at_arrest_cooperative,
-         reason_injury = search_reason_cause_injury,
+  select(-c(x_id, arrest_id, person_id, object_id, booked, actions_at_arrest_concealed_i, actions_at_arrest_combative, actions_at_arrest_resisted_d, actions_at_arrest_mental_inst, actions_at_arrest_assaulted_o, actions_at_arrest_cooperative)) |> # Get rid of unnecessary variables
+  rename(
+         reason_injury = search_reason_cause_injury, # Shorten variable names to make them readable
          reason_escape = search_reason_assist_escape,
          reason_weapons = search_reason_possess_weapons,
          reason_possess_evidence = search_reason_possess_evidence,
@@ -48,9 +43,9 @@ cleaned_arrests_strip_data <-
   naniar::replace_with_na(replace = list(reason_injury = c("None"), # Replace the string 'None' with NA to make it easier to manipulate data later on
                                  reason_escape = c("None"),
                                  reason_weapons = c("None"),
-                                 reason_has_evidence = c("None")))
+                                 reason_possess_evidence = c("None")))
 
-# Replace all "XX" and "None" from the columns arrest_loc_div and items_found with NA and NA respectively to make the data processing more efficient later on
+# Replace all "XX" and "None" from the columns arrest_loc_div and items_found with "NA" respectively to make the data processing more efficient later on
 # Code referenced from https://www.youtube.com/watch?v=g1eppE60J5g
 cleaned_arrests_strip_data$items_found[cleaned_arrests_strip_data$items_found == "None"] <- NA
 cleaned_arrests_strip_data$arrest_loc_div[cleaned_arrests_strip_data$arrest_loc_div == "XX"] <- NA
@@ -68,10 +63,10 @@ cleaned_search_reasons_data =
   select(event_id, reason_injury,	reason_escape, reason_weapons, reason_possess_evidence) |>
   filter(!is.na(reason_injury))
 
+# Replace all "NA" and "1" from the column strip_search with "No" and "Yes" respectively to make the data clearer later on
+cleaned_race_gender_data$strip_search[is.na(cleaned_race_gender_data$strip_search)] <- "No"
+cleaned_race_gender_data$strip_search[cleaned_race_gender_data$strip_search == 1] <- "Yes"
 
-  cleaned_arrests_strip_data |>
-  select(event_id, reason_injury,	reason_escape, reason_weapons, reason_possess_evidence, items_found) |>
-  filter(reason_injury == 0 & reason_escape == 0 & reason_weapons == 0 & reason_possess_evidence == 0)
 
 #### Save data ####
 write_csv(cleaned_arrests_strip_data,"inputs/data/cleaned_arrests_strip_data.csv")

6153494d01718d67f65a390109f7430f201daa5c
ponolite <147739831+ponolite@users.noreply.github.com>
2024-01-26 03:21:38 -0500

    Update 03-test_data.R

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,123 +1,75 @@
 #### Preamble ####
-# Purpose: Tests cleaned data sets 
-# Author: Thomas Fox 
-# Date: 25 January 2024
-# Contact: thomas.fox@mail.utoronto.ca
+# Purpose: Tests cleaned datasets 
+# Author: Quang Mai
+# Date: 24 January 2024
+# Contact: q.mai@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: run 01-download_data.R and 02-data_cleaning.R
-# Any other information needed? If nothing is printed, all tests have passed
-
+# Pre-requisites: reload 01-download_data.R and 02-data_cleaning.R
 
 #### Workspace setup ####
 library(tidyverse)
 library(dplyr)
 library(janitor)
 
-#### Load Cleaned Data Sets ####
 
-# read cleaned child care data
-test_child_care = read_csv(
-  file = "outputs/data/child_care_data.csv",
+#### Read in cleaned data ####
+
+
+# read cleaned data on race and gender and strip searches
+cleaned_race_gender_data = read_csv(
+  file = here("inputs/data/cleaned_race_gender_data.csv"),
   show_col_types = FALSE
 )
 
-# read cleaned census data
-test_census_data = read_csv(
-  file = "outputs/data/census_data.csv",
-  show_col_types = FALSE
-)
-
-# read cleaned merged child care/census data
-test_census_childcare_data = read_csv(
-  file = "outputs/data/merged_ward_data.csv",
-  show_col_types = FALSE
-)
-
-# read ward names
-test_ward_names = read_csv(
-  file = "outputs/data/ward_names.csv",
+# read cleaned data on reasons for strip search
+cleaned_search_reasons_data = read_csv(
+  file = here("inputs/data/cleaned_search_reasons_data.csv"),
   show_col_types = FALSE
 )
 
 
 #### Test data ####
 
-# Test cleaned child care data 
+test1
 
-if (nrow(test_child_care) != 1063) {
-  print("Size of child care data is incorrect ")
-}
+# Check that there are between 1 and 65276 events of police arrests and strip searches in the cleaned dataset on gender and race 
+cleaned_race_gender_data$event |> min() == 1
+cleaned_race_gender_data$event |> max() == 65276
 
-if (min(test_child_care$totspace) < 0) {
-  print("Negative number in child care data total space column")
-}
+test2
 
-if (min(test_child_care$ward) <= 0) {
-  print("A ward number is below the limits ward number limit (1-25)")
-}
+# Check that the dataset on race and gender of strip searches prolongs from 2020 to 2021 
+cleaned_race_gender_data$arrest_year |> min() == 2020
+cleaned_race_gender_data$arrest_year |> max() == 2021
 
-if (max(test_child_care$ward) > 25) {
-  print("A ward number is above the ward number limit (1-25)")
-}
+test3
 
-# Test cleaned child care data 
+# Check that there are 9 races identified in the cleaned dataset on gender, race and strip search
+cleaned_race_gender_data$race |> unique() |> length() == 9
 
-if (nrow(test_census_data) != 25) {
-  print("Number of ward entries is incorrect")
-}
+test4
 
-if (min(test_census_data$avg_hh_income) < 0) {
-  print("Negative number in average house hold income column")
-}
+# Check that there are 3 gender identities identified in the cleaned dataset on gender, race and strip search
+cleaned_race_gender_data$gender |> unique() |> length() == 3
 
-if (min(test_census_data$pop_0_to_4) < 0) {
-  print("Negative number in population 0 to 4 column")
-}
+test5
 
-if (min(test_census_data$ward) <= 0) {
-  print("A ward number is below the limits ward number limit (1-25)")
-}
+# Check that there are exactly 2 options of "Yes" and "No" for strip searches #
+cleaned_race_gender_data$strip_search |> unique() |> length() == 2
 
-if (max(test_census_data$ward) > 25) {
-  print("A ward number is above the ward number limit (1-25)")
-}
+tes6
+
+# Check that there are 7801 observations in the cleaned dataset on reasons for strip search
+cleaned_search_reasons_data$id |> unique() |> length() == 7801
+
+test7
+# Check that there are maximum 4 total reasons and minimum 0 total reasons in the cleaned dataset on reasons for strip search
+cleaned_search_reasons_data$total_reasons |> min() == 0
+cleaned_search_reasons_data$total_reasons |> max() == 4
 
 
-# Test merged cleaned child care and census data 
-
-if (nrow(test_census_childcare_data) != 25) {
-  print("Number of ward entries is incorrect")
-}
-
-if (min(test_census_childcare_data$ward) <= 0) {
-  print("A ward number is below the limits ward number limit (1-25)")
-}
-
-if (max(test_census_childcare_data$ward) > 25) {
-  print("A ward number is above the ward number limit (1-25)")
-}
-
-if (min(test_census_childcare_data$avg_hh_income) < 0) {
-  print("Negative number in average house hold income column")
-}
-
-if (min(test_census_childcare_data$pop_0_to_4) < 0) {
-  print("Negative number in population 0 to 4 column")
-}
-
-# Test ward names 
-
-if (nrow(test_ward_names) != 25) {
-  print("Number of ward entries is incorrect")
-}
-
-if (class(test_ward_names$ward_name) != "character") {
-  print("Ward names is not a character")
-}
-
-if (class(test_ward_names$ward_number) != "numeric") {
-  print("Ward numbers are not numeric")
-}
-
+test8
+# Check that there are only arrests with strip searches in the cleaned dataset on reasons for strip search
+cleaned_search_reasons_data$total_reasons |> unique |> length() >= 0
 
 
